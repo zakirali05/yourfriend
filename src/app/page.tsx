1f3ec8affa15d27d4685c2050f4e9ccf519626b7
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2024-10-04 12:39:33 +0530

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import { ARRAY_OF_VIDEOS } from "@/constants";
 
 export default function Home() {
   const RANDOM_VIDEO = Math.floor(Math.random() * 8) + 1;
+  console.log(RANDOM_VIDEO - 1);
   return (
     <main className="flex justify-center min-h-screen  relative overflow-hidden">
       <video
-        src={ARRAY_OF_VIDEOS[RANDOM_VIDEO]}
+        src={ARRAY_OF_VIDEOS[RANDOM_VIDEO - 1]}
         autoPlay={true}
         loop
         muted
         className="absolute z-10 w-auto 
             min-w-full min-h-full max-w-none"
       ></video>
-      <MainContainer />
+      <MainContainer random_video={RANDOM_VIDEO} />
     </main>
   );
 }
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,10 +2,20 @@
 import { INITIAL_AYAH } from "@/constants";
 import { getRandomAyat } from "@/utils/getRandomAyat";
 import { useState } from "react";
+import {
+  BUTTON_COLORS_ACCORDING_TO_VIDEO,
+  TEXT_COLOR_ACCORDING_TO_VIDEO,
+} from "@/constants";
+import { useRouter } from "next/navigation";
+import { cn } from "@/utils/cn";
 
-const MainContainer = () => {
+const MainContainer = ({ random_video }: { random_video: number }) => {
   const [ayat, setAyat] = useState<any>(INITIAL_AYAH);
   const [loading, setLoading] = useState(false);
+  const TEXT_COLOUR = TEXT_COLOR_ACCORDING_TO_VIDEO[random_video - 1];
+  const BG_COLOUR = BUTTON_COLORS_ACCORDING_TO_VIDEO[random_video - 1];
+  console.log(TEXT_COLOUR, BG_COLOUR);
+  const router = useRouter();
   const getAyat = async () => {
     try {
       setLoading(true);
@@ -19,35 +29,58 @@ const MainContainer = () => {
     }
   };
   return (
-    <div className="p-4  backdrop:blur-md z-[1000] rounded-md shadow-lg w-[600px] min-h-[450px] max-h-[500px] mt-16 flex flex-col">
-      <div className="flex flex-col gap-2 max-h-[300px] p-1 overflow-y-scroll">
-        <p className="text-center">
-          {ayat?.surahName} - [{ayat?.surahNameTranslation}]
-        </p>
-        <p className="text-center">
-          ({ayat?.ayahNo}:{ayat?.surahNo})
-        </p>
-        <h1 className="my-3 font-bold text-2xl text-right">{ayat?.arabic1}</h1>
-        <p className="text-center">{ayat?.english}</p>
-      </div>
-
-      <div className="flex flex-col gap-4 items-center justify-center w-full mt-auto ">
-        <audio
-          src={ayat?.audio["1"]?.url}
-          typeof="audio/mp3"
-          controls
-          className="w-full "
-        />
-
-        <button
-          disabled={loading}
-          onClick={getAyat}
-          className="rounded-md w-full p-3 bg-purple-800 disabled:cursor-not-allowed hover:opacity-70 disabled:opacity-70"
+    <>
+      <button
+        disabled={loading}
+        onClick={() => router.refresh()}
+        className={cn(
+          ` absolute top-3 right-3 text-xs rounded-md shadow-lg   p-2 
+          disabled:cursor-not-allowed hover:opacity-70 disabled:opacity-70 z-[1000]`,
+          BG_COLOUR
+        )}
+      >
+        Change BG
+      </button>
+      <div className="p-4   backdrop:blur-sm z-[1000] rounded-md shadow-lg w-[600px] h-[480px]  mt-16 flex flex-col">
+        <div
+          className={cn(
+            `flex  flex-col gap-2 max-h-[300px]   p-1 overflow-y-scroll font-bold`,
+            TEXT_COLOUR
+          )}
         >
-          {loading ? "Loading..." : "Talk to me my lord 😊"}
-        </button>
+          <p className="text-center">
+            {ayat?.surahName} - [{ayat?.surahNameTranslation}]
+          </p>
+          <p className="text-center">
+            ({ayat?.ayahNo}:{ayat?.surahNo})
+          </p>
+          <h1 className="my-3 font-bold text-3xl text-right">
+            {ayat?.arabic1}
+          </h1>
+          <p className="text-center text-2xl">"{ayat?.english}"</p>
+        </div>
+
+        <div className="flex flex-col gap-4 items-center justify-center w-full mt-auto ">
+          <audio
+            src={ayat?.audio["1"]?.url}
+            typeof="audio/mp3"
+            controls
+            className="w-full"
+          />
+
+          <button
+            disabled={loading}
+            onClick={getAyat}
+            className={cn(
+              `rounded-md shadow-lg  w-full p-3  disabled:cursor-not-allowed hover:opacity-70 disabled:opacity-70`,
+              BG_COLOUR
+            )}
+          >
+            {loading ? "Loading..." : "Talk to me my lord 😊"}
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
